4be2f2ead27f0156d7743ddcba6a83d4d75c6947
Elasfer-Abdellah <abdellah.elasfer.56@edu.uiz.ac.ma>
2025-06-11 22:07:08 +0100

    Create Point2DWritable.java

---
+++ Point2DWritable.java
@@ -0,0 +1,55 @@
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+import org.apache.hadoop.io.Writable;
+import java.awt.geom.Point2D;
+
+public class Point2DWritable extends Point2D.Double implements Writable {
+  
+    public Point2DWritable() {
+        super(0.0, 0.0);
+    }
+
+    public Point2DWritable(double x, double y) {
+        super(x, y);
+    }
+
+    public Point2DWritable(Point2D.Double point) {
+        super(point.x, point.y);
+    }
+
+    // Surdefinition des méthodes d'interface Writable
+    @Override
+    public void write(DataOutput out) throws IOException {
+        out.writeDouble(x);
+        out.writeDouble(y);
+    }
+
+    @Override
+    public void readFields(DataInput in) throws IOException {
+        x = in.readDouble();
+        y = in.readDouble();
+    }
+
+    // Surdefinition de la méthode d'affichage d'output
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+
+    // Surdefinition des méthodes de comparaison
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point2DWritable obj = (Point2DWritable) o;
+        return Double.compare(obj.x, x) == 0 && Double.compare(obj.y, y) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        long xBits = Double.doubleToLongBits(x);
+        long yBits = Double.doubleToLongBits(y);
+        return (int) (xBits ^ (xBits >>> 32) ^ yBits ^ (yBits >>> 32));
+    }
+}
